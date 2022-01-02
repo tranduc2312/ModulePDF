98520ef8ca31c6fb1c34e4acbb65ffc93d5b9630
Trần Đức <tranduc2312@gmail.com>
2022-01-02 20:42:47 +0700

    init project

---
+++ src/ModulePDF/ModulePDF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ModulePDF.Startup))]
+namespace ModulePDF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
